490442b13fdafea21c26d203e3a234d9ba8d4b62
narirou <narirou@gmail.com>
2013-11-28 23:01:31 +0900

    Add Unit Test

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,5 @@
 var fs     = require( 'fs' ),
-	assert = require( 'assert' ),
+	should = require( 'should' ),
 	jconv  = require( __dirname + '/../' );
 
 function getBuffer( type, name ) {
@@ -12,25 +12,24 @@ function check( type, from, to ) {
 		var fromBuf = getBuffer( type, from );
 		var convertedBuf = jconv.convert( fromBuf, from, to );
 
-		// console.log( '' );
-		// console.log( toBuf );
-		// console.log( convertedBuf );
-
-		assert.deepEqual( toBuf, convertedBuf );
+		should( convertedBuf ).eql( toBuf );
 	});
 }
 
+// jconv.decode, jconv.encode function
 describe( 'jconv.decode, jconv.encode', function() {
 	it( '#iconv-lite Function', function() {
-		var input = '君知るや南の国 かなたへ 君と共にゆかまし';
-		var jisBuffer = jconv.encode( input, 'JIS' );
+		var inputString = '君よ知るや南の国';
+		var jisBuffer = jconv.encode( inputString, 'JIS' );
 		var utfString = jconv.decode( jisBuffer, 'JIS' );
 
-		assert.equal( input, utfString );
+		should( utfString ).eql( inputString )
 	});
 });
 
-describe( 'jconv should convert KANJI', function() {
+// KANJI
+// あ	亜	弌
+describe( 'jconv.convert KANJI', function() {
 	check( 'KANJI', 'UTF8', 'SJIS' );
 	check( 'KANJI', 'UTF8', 'JIS' );
 	check( 'KANJI', 'UTF8', 'EUCJP' );
@@ -48,7 +47,9 @@ describe( 'jconv should convert KANJI', function() {
 	check( 'KANJI', 'EUCJP', 'JIS' );
 });
 
-describe( 'jconv should convert ASCII', function() {
+// ASCII
+// abcd	ABCD	0123
+describe( 'jconv.convert ASCII', function() {
 	check( 'ASCII', 'UTF8', 'SJIS' );
 	check( 'ASCII', 'UTF8', 'JIS' );
 	check( 'ASCII', 'UTF8', 'EUCJP' );
@@ -66,7 +67,9 @@ describe( 'jconv should convert ASCII', function() {
 	check( 'ASCII', 'EUCJP', 'JIS' );
 });
 
-describe( 'jconv should convert HALFWIDTH_KATAKANA', function() {
+// HALFWIDTH_KATAKANA
+// ｱｲｳｴｵ
+describe( 'jconv.convert HALFWIDTH_KATAKANA', function() {
 	check( 'HALFWIDTH_KATAKANA', 'UTF8', 'SJIS' );
 	check( 'HALFWIDTH_KATAKANA', 'UTF8', 'JIS' );
 	check( 'HALFWIDTH_KATAKANA', 'UTF8', 'EUCJP' );
@@ -84,10 +87,10 @@ describe( 'jconv should convert HALFWIDTH_KATAKANA', function() {
 	check( 'HALFWIDTH_KATAKANA', 'EUCJP', 'JIS' );
 });
 
-describe( 'jconv should convert WindowsDependent', function() {
-	// toSJIS Converting is convert IBM EXTENSION to NEC's select IBM EXTENSION.
-
-	// check( 'WindowsDependent', 'UTF8', 'SJIS' );
+// Windows Dependent
+// 纊	ⅰ	㈱	①
+describe( 'jconv.convert WindowsDependent', function() {
+	check( 'WindowsDependent', 'UTF8', 'SJIS' );
 	check( 'WindowsDependent', 'UTF8', 'JIS' );
 	check( 'WindowsDependent', 'UTF8', 'EUCJP' );
 
@@ -104,9 +107,110 @@ describe( 'jconv should convert WindowsDependent', function() {
 	check( 'WindowsDependent', 'EUCJP', 'JIS' );
 });
 
-describe( 'jconv should convert EXTENSION', function() {
-	// SJIS does not have EXTENSION table.
+// WAVE DASH
+// ～
+describe( 'jconv.convert WAVEDASH', function() {
+	check( 'WAVEDASH', 'UTF8', 'SJIS' );
+	check( 'WAVEDASH', 'UTF8', 'JIS' );
+	check( 'WAVEDASH', 'UTF8', 'EUCJP' );
 
+	check( 'WAVEDASH', 'SJIS', 'UTF8' );
+	check( 'WAVEDASH', 'SJIS', 'JIS' );
+	check( 'WAVEDASH', 'SJIS', 'EUCJP' );
+
+	check( 'WAVEDASH', 'JIS', 'UTF8' );
+	check( 'WAVEDASH', 'JIS', 'SJIS' );
+	check( 'WAVEDASH', 'JIS', 'EUCJP' );
+
+	check( 'WAVEDASH', 'EUCJP', 'UTF8' );
+	check( 'WAVEDASH', 'EUCJP', 'SJIS' );
+	check( 'WAVEDASH', 'EUCJP', 'JIS' );
+});
+
+// SIGN
+// ￠	￡	￢	￤	＇	＂
+describe( 'jconv.convert SIGN', function() {
+	check( 'SIGN', 'UTF8', 'SJIS' );
+	check( 'SIGN', 'UTF8', 'JIS' );
+	check( 'SIGN', 'UTF8', 'EUCJP' );
+
+	check( 'SIGN', 'SJIS', 'UTF8' );
+	check( 'SIGN', 'SJIS', 'JIS' );
+	check( 'SIGN', 'SJIS', 'EUCJP' );
+
+	check( 'SIGN', 'JIS', 'UTF8' );
+	// check( 'SIGN', 'JIS', 'SJIS' );
+	check( 'SIGN', 'JIS', 'EUCJP' );
+
+	check( 'SIGN', 'EUCJP', 'UTF8' );
+	// check( 'SIGN', 'EUCJP', 'SJIS' );
+	check( 'SIGN', 'EUCJP', 'JIS' );
+});
+
+// SIGN-YEN
+// \	/
+describe( 'jconv.convert SIGN-YEN', function() {
+	check( 'SIGN-YEN', 'UTF8', 'SJIS' );
+	check( 'SIGN-YEN', 'UTF8', 'JIS' );
+	check( 'SIGN-YEN', 'UTF8', 'EUCJP' );
+
+	check( 'SIGN-YEN', 'SJIS', 'UTF8' );
+	check( 'SIGN-YEN', 'SJIS', 'JIS' );
+	check( 'SIGN-YEN', 'SJIS', 'EUCJP' );
+
+	check( 'SIGN-YEN', 'JIS', 'UTF8' );
+	check( 'SIGN-YEN', 'JIS', 'SJIS' );
+	check( 'SIGN-YEN', 'JIS', 'EUCJP' );
+
+	check( 'SIGN-YEN', 'EUCJP', 'UTF8' );
+	check( 'SIGN-YEN', 'EUCJP', 'SJIS' );
+	check( 'SIGN-YEN', 'EUCJP', 'JIS' );
+});
+
+// PARALLEL TO
+// ∥
+describe( 'jconv.convert PARALLELTO', function() {
+	check( 'PARALLELTO', 'UTF8', 'SJIS' );
+	check( 'PARALLELTO', 'UTF8', 'JIS' );
+	check( 'PARALLELTO', 'UTF8', 'EUCJP' );
+
+	check( 'PARALLELTO', 'SJIS', 'UTF8' );
+	check( 'PARALLELTO', 'SJIS', 'JIS' );
+	check( 'PARALLELTO', 'SJIS', 'EUCJP' );
+
+	check( 'PARALLELTO', 'JIS', 'UTF8' );
+	check( 'PARALLELTO', 'JIS', 'SJIS' );
+	check( 'PARALLELTO', 'JIS', 'EUCJP' );
+
+	check( 'PARALLELTO', 'EUCJP', 'UTF8' );
+	check( 'PARALLELTO', 'EUCJP', 'SJIS' );
+	check( 'PARALLELTO', 'EUCJP', 'JIS' );
+});
+
+// SIGN-MINUS
+// ―	－
+describe( 'jconv.convert SIGN-MINUS', function() {
+	check( 'SIGN-MINUS', 'UTF8', 'SJIS' );
+	check( 'SIGN-MINUS', 'UTF8', 'JIS' );
+	check( 'SIGN-MINUS', 'UTF8', 'EUCJP' );
+
+	check( 'SIGN-MINUS', 'SJIS', 'UTF8' );
+	check( 'SIGN-MINUS', 'SJIS', 'JIS' );
+	check( 'SIGN-MINUS', 'SJIS', 'EUCJP' );
+
+	check( 'SIGN-MINUS', 'JIS', 'UTF8' );
+	check( 'SIGN-MINUS', 'JIS', 'SJIS' );
+	check( 'SIGN-MINUS', 'JIS', 'EUCJP' );
+
+	check( 'SIGN-MINUS', 'EUCJP', 'UTF8' );
+	check( 'SIGN-MINUS', 'EUCJP', 'SJIS' );
+	check( 'SIGN-MINUS', 'EUCJP', 'JIS' );
+});
+
+// EXTENSION
+// SJIS does not have the EXTENSION-table(JIS X 0212).
+// 丂	‖	−
+describe( 'jconv.convert EXTENSION', function() {
 	check( 'EXTENSION', 'UTF8', 'JIS' );
 	check( 'EXTENSION', 'UTF8', 'EUCJP' );
 
@@ -116,11 +220,3 @@ describe( 'jconv should convert EXTENSION', function() {
 	check( 'EXTENSION', 'EUCJP', 'UTF8' );
 	check( 'EXTENSION', 'EUCJP', 'JIS' );
 });
-
-// WAVEDASH
-
-// SPECIFIC SIGN
-
-// PARALLEL TO
-
-// DASH
